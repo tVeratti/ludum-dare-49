072309253d67c20eeeb96ba720aea7adf99ce332
Tatiana Veratti <TatianaVeratti@gmail.com>
2021-10-04 15:01:12 -0700

    fix symbol mapping

--- src/infrastructure/symbols.gd
+++ src/infrastructure/symbols.gd
@@ -5,7 +5,8 @@ const FaceLoathing = preload("res://assets/textures/face_anger_2.png")
 const FaceRage = preload("res://assets/textures/face_anger_3.png")
 
 const FaceConfidence = preload("res://assets/textures/face_fear_1.png")
-const FaceAdmiration = preload("res://assets/textures/face_fear_2.png")
+const FaceVigilance = preload("res://assets/textures/face_fear_2.png")
+const FaceAdmiration = preload("res://assets/textures/admirationamybe.png")
 const FaceTerror = preload("res://assets/textures/face_fear_3.png")
 
 const FaceHappy = preload("res://assets/textures/face_sad_1.png")
@@ -18,7 +19,7 @@ static func get_symbol(type:int):
 	match(type):
 		EmotionScale.TYPES.RAGE: return FaceRage
 		EmotionScale.TYPES.TERROR: return FaceTerror
-		EmotionScale.TYPES.VIGILANCE: return FaceConfidence
+		EmotionScale.TYPES.VIGILANCE: return FaceVigilance
 		EmotionScale.TYPES.AMAZEMENT: return FaceAmaze
 		EmotionScale.TYPES.ECSTASY: return FaceHappy
 		EmotionScale.TYPES.GRIEF: return FaceSad3
